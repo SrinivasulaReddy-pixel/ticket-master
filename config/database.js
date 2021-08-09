a843dd826d79f7225f822bdada0944d679ba67c7
srinivas <balusrinivasuareddy@gmail.com>
2021-08-09 12:14:24 +0530

    initial commiit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,31 @@
+// const mongoose = require('mongoose');
+
+// const setupDB = () => {
+//   mongoose
+//     .connect('mongodb+srv://cluster0.hagxy.mongodb.net/myFirstDatabase')
+//     //.connect('mongodb://localhost:27017/ticket-master')
+//     .then(() => {
+//       console.log('connected to db');
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+// module.exports = setupDB;
+
 const mongoose = require('mongoose');
 
-const setupDB = () => {
-  mongoose
-    .connect('mongodb+srv://cluster0.hagxy.mongodb.net/myFirstDatabase')
-    //.connect('mongodb://localhost:27017/ticket-master')
-    .then(() => {
-      console.log('connected to db');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+mongoose.Promise = global.Promise;
+const CONNECTION_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/ticket-master';
+mongoose
+  .connect(CONNECTION_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('succesfully connected to db');
+  })
+  .catch(() => {
+    console.log('error connecting to db');
+  });
 
-module.exports = setupDB;
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,27 +1,50 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const setupDB = require('./config/database');
+// const router = require('./config/routes');
+// const cors = require('cors');
+// const app = express();
+
+// //const port = 3010
+// //deployment changes start
+// const port = process.env.PORT || 3000;
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
+// //end of deployment changes
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/', router);
+// //db configuration
+// setupDB();
+
+// app.listen(port, () => {
+//   console.log('listening port', port);
+// });
+
 const express = require('express');
-const mongoose = require('mongoose');
-const setupDB = require('./config/database');
+const mongoose = require('./config/database');
 const router = require('./config/routes');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 3001;
 
-//const port = 3010
-//deployment changes start
-const port = process.env.PORT || 3000;
 const path = require('path');
+app.use(express.json());
+app.use(cors());
+mongoose()
+
+app.use('/', router);
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
-//end of deployment changes
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/', router);
-//db configuration
-setupDB();
 
 app.listen(port, () => {
-  console.log('listening port', port);
+  console.log('listening on port ', port);
 });
